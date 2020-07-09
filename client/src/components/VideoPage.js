e7d5ec47b50bf98a29ac93e79b9bf32c7118cd6a
Shoeb Joarder <shoebjoarder@gmail.com>
2020-07-09 16:04:40 +0200

    Revert "experiment with quiz as component"
    This reverts commit e6780fd622dba933a69e68c8698dbdf86ac2711b.

--- client/src/components/VideoPage.js
+++ client/src/components/VideoPage.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Container, Col, Row, Image, Button } from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactPlayer from "react-player"
-import Quiz from './Quiz'
 
 
 export default class VideoPage extends React.Component {
@@ -10,9 +9,7 @@ export default class VideoPage extends React.Component {
 		super(props)
 		this.state = {
 			title: "",
-			video: false,
-			videoNumber: 0,
-			quizTime: false
+			video: false
 		}
 	}
 	componentDidMount() {
@@ -49,40 +46,29 @@ export default class VideoPage extends React.Component {
 		)
 
 		const ButtonQuiz = (
-			<div className={"row justify-content-end"} style={{ marginTop: '2.7em', marginRight: '0.6em' }}>
+			<div className={"row justify-content-end"} style={{ marginTop:'2.7em', marginRight:'0.6em'}}>
 				<Button onClick={this.handleQuiz} size="lg" block style={{ borderRadius: '0.7em', backgroundColor: '#1E38BF', boxShadow: '2px 2px 4px #000000', fontSize: '1.5em', width: '8em' }}>Take quiz!</Button>
 			</div>
 		)
 
-		const VideoPlayer = (
-			<div>
-				<Button onClick={this.handleCourse} variant="outline-light" style={{ fontSize: '1.3em', backgroundColor: '#FFFFFF', color: "#8A8A8A" }} >
-					&lt; Back to courses
-						</Button>
-				<p style={{ paddingBottom: '1em' }}></p>
-				{this.state.video ? Video : noVideo}
-				{this.state.video ? ButtonQuiz : null}
-			</div>
-		)
-
-		const itsQuizTime = (
-			<div>
-				<Quiz />
-			</div>
-		)
 		return (
 			<Container>
 				<Row style={{ marginTop: "1.75em", marginBottom: '11.4em' }}>
-					<Col className="align-self-center" style={{ border: "1px solid #fff", padding: "50px 60px", backgroundColor: '#fff', borderRadius: '0.8em', marginTop: "3em", WebkitBoxShadow: "0px 0px 20px -1px rgba(0,0,0,0.75)", MozBoxShadow: "0px 0px 20px -1px rgba(0,0,0,0.75)", boxShadow: '0px 0px 20px -1px rgba(0,0,0,0.75)' }}>
+					<Col className="align-self-center" style={{ border: "1px solid #fff", padding: "50px 60px", backgroundColor: '#fff', borderRadius: '0.8em', marginTop: "3em", WebkitBoxShadow: "0px 0px 20px -1px rgba(0,0,0,0.75)", MozBoxShadow: "0px 0px 20px -1px rgba(0,0,0,0.75)",  boxShadow: '0px 0px 20px -1px rgba(0,0,0,0.75)' }}>
 
 						<p style={{ fontSize: '3em' }}>{this.state.title}</p>
+						<Button onClick={this.handleCourse} variant="outline-light" style={{ fontSize: '1.3em', backgroundColor: '#FFFFFF', color: "#8A8A8A" }} >
+							&lt; Back to courses
+						</Button>
 
+						<p style={{ paddingBottom: '1em' }}></p>
 
-						{this.state.quizTime ? itsQuizTime : VideoPlayer}
+						{this.state.video ? Video : noVideo}
+						{this.state.video ? ButtonQuiz : null}
+					</Col>
+				</Row>
 
-					</Col >
-				</Row >
-			</Container >
+			</Container>
 		)
 	}
 }
